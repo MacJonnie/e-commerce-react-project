e44a562b7a5449b503f88ba02a0ca1304098e1e4
Mark John <markjohn165@gmail.com>
2026-05-06 16:01:51 +0100

    Add Auth

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,8 +13,8 @@ export default function Navbar(){
                 </div>
                 <div className="navbar-auth">
                     <div className="navbar-auth-links">
-                        <Link to="/auth" className="btn btn-secondary">Login</Link>             
-                        <Link to="/auth" className="btn btn-primary">Signup</Link>             
+                        <Link to="/auth" className="btn btn-secondary">Login</Link>
+                        <Link to="/auth" className="btn btn-primary">Signup</Link>
                     </div>
                 </div>
             </div>
